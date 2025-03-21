28f9568bb982a86f6643c6e9d12f23cf76316ced
supersliser <thomaslower9@gmail.com>
2025-03-21 22:50:39 +0000

    moving to uni plz ignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/programming-assignment-supersliser
-# Build directory: /home/tom/programming-assignment-supersliser/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/tom/programming-assignment-supersliser/cmake-build-debug/Tests[1]_include.cmake")
