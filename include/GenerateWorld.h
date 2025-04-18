a9d67958f12a06131a5819f976c79881c9e9b362
supersliser <thomaslower9@gmail.com>
2025-04-18 11:21:56 +0100

    trees

--- include/GenerateWorld.h
+++ include/GenerateWorld.h
@@ -32,4 +32,6 @@ void generateWorld(Table* i_world, int i_xsize, int i_ysize, int i_zsize, siv::P
 /// @return The noise value at the specified point between 0 - 1.
 float getNoise(siv::PerlinNoise noise, int x, int y, float scale);
 
+void growTree(Table* i_world, int i_xPos, int i_zPos, int i_yPos, int* i_blockCount);
+
 #endif //COLONYMANAGERBUILD_GENERATEWORLD_H
--- src/GenerateWorld.cpp
+++ src/GenerateWorld.cpp
@@ -6,7 +6,7 @@
 
 void generateWorld(Table* i_world, int i_xsize, int i_ysize, int i_zsize, siv::PerlinNoise::seed_type i_seed)
 {
-    float scale = 0.0001f;
+    float scale = 0.025f;
     siv::PerlinNoise noise{i_seed};
     int blockCount = 0;
     for (int x = 0; x < i_xsize; x++)
@@ -59,13 +59,50 @@ void generateWorld(Table* i_world, int i_xsize, int i_ysize, int i_zsize, siv::P
                 setPositionSystem.i_pos = ngl::Vec3(x, y, z);
                 i_world->run(&setPositionSystem, TransformComponents::getComponentID(), blockCount, blockCount);
                 blockCount++;
-                printf("Block %d: %d %d %d\n", blockCount, x, y, z);
             }
         }
+    }
+
+    for (int i = 0; i < (i_xsize + i_zsize) / 2; i++)
+    {
+        int xPos = random() % i_xsize;
+        int zPos = random() % i_zsize;
+        int yPos = (int)(i_ysize * getNoise(noise, xPos, zPos, scale));
+        growTree(i_world, xPos, zPos, yPos, &blockCount);
     }
 }
 
 float getNoise(siv::PerlinNoise noise, int x, int y, float scale)
 {
     return std::clamp((float)noise.noise2D(x * scale, y * scale) * 0.5f + 0.5f, 0.2f, 1.0f);
+}
+
+void growTree(Table* i_world, int i_xPos, int i_zPos, int i_yPos, int* i_blockCount)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        i_world->createEntity();
+        ApplyBlockTextureSystem applyBlockTextureSystem;
+        applyBlockTextureSystem.i_blockType = BlockType::Log;
+        i_world->run(&applyBlockTextureSystem, BlockTextureComponent::getComponentID(), *i_blockCount, *i_blockCount);
+        SetPositionSystem setPositionSystem;
+        setPositionSystem.i_pos = ngl::Vec3(i_xPos, i_yPos + i, i_zPos);
+        i_world->run(&setPositionSystem, TransformComponents::getComponentID(), *i_blockCount, *i_blockCount);
+        (*i_blockCount)++;
+    }
+
+    for (int y = 3; y < 7; y++) {
+        for (int x = -2; x < 3; x++) {
+            for (int z = -2; z < 3; z++) {
+                i_world->createEntity();
+                ApplyBlockTextureSystem applyBlockTextureSystem;
+                applyBlockTextureSystem.i_blockType = BlockType::Leaves;
+                i_world->run(&applyBlockTextureSystem, BlockTextureComponent::getComponentID(), *i_blockCount, *i_blockCount);
+                SetPositionSystem setPositionSystem;
+                setPositionSystem.i_pos = ngl::Vec3(i_xPos + x, y + i_yPos, i_zPos + z);
+                i_world->run(&setPositionSystem, TransformComponents::getComponentID(), *i_blockCount, *i_blockCount);
+                (*i_blockCount)++;
+            }
+        }
+    }
 }
