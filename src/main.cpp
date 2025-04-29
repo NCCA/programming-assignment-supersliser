27ba84d5489d517a3af07db1eeee1887c1f61664
supersliser <thomaslower9@gmail.com>
2025-04-29 22:15:19 +0100

    little message for labs

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char* argv[])
     SDLWindowManager windowManager = SDLWindowManager(false, true, true, true, true, true, true, true, 1, ngl::Vec3(0, 20, 0));
     windowManager.createWindow("World Generator TEST");
     windowManager.generateWorld = [](Table* i_world, Table* i_players) {
+        //The value here can be up to (100, 20, 100) if in BU Labs
         generateWorld(i_world, 20, 10, 20, 1565614156156);
     };
     windowManager.runEvents();
